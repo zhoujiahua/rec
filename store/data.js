351fda22d407b6deea91318a1c433ae6b347dcb1
jerryZhou <jerry.zhou@ptn.advantest.com>
2021-04-26 11:10:26 +0800

    Update file

--- store/data.js
+++ store/data.js
@@ -1,173 +1,216 @@
-const work = {
-    "isCold": "否",
-    "seeDoctor": "",
-    "businessTravel": "否",
-    "returnTools": "",
-    "leaveCompanyLocation": "否",
-    "coldFamily": "",
-    "leaveWay": "",
-    "isolate": "否",
-    "isWorking": "是",
-    "familyIsHome": "",
-    "overseas": "否",
-    "temperature": "36.5",
-    "alreadyReturn": "",
-    "name": "周家华",
-    "englishName": "JerryZhou",
-    "phone": "17629269055",
-    "idCard": "612427199107220010",
-    "company": "西安华讯",
-    "department": "STS",
-    "team": "ADV-RND",
-    "location": "陕西省西安市长安区韦曲北街风憬天下西区12号楼",
-    "holidayRegion": "陕西省西安市长安区韦曲北街风憬天下西区12号楼",
-    "workWay": "公司笔记本或公司PC电脑",
-    "workType": "Office值班/协作(工作)",
-    "feiyanIsTouch": "否",
-    "isPassGd": "否",
-    "help": "无",
-    "coldReason": "",
-    "coldDate": "",
-    "hospitalName": "",
-    "noWorkReason": "",
-    "trainNumber": "",
-    "overseasReason": "",
-    "isolateInfo": ""
-}
-
-const weekend = {
-    "isCold": "否",
-    "seeDoctor": "",
-    "businessTravel": "否",
-    "returnTools": "",
-    "leaveCompanyLocation": "否",
-    "coldFamily": "",
-    "leaveWay": "",
-    "isolate": "否",
-    "isWorking": "否",
-    "familyIsHome": "",
-    "overseas": "否",
-    "temperature": "36.5",
-    "alreadyReturn": "",
-    "name": "周家华",
-    "englishName": "JerryZhou",
-    "phone": "17629269055",
-    "idCard": "612427199107220010",
-    "company": "西安华讯",
-    "department": "STS",
-    "team": "ADV-RND",
-    "location": "陕西省西安市长安区韦曲北街风憬天下西区12号楼",
-    "holidayRegion": "陕西省西安市长安区韦曲北街风憬天下西区12号楼",
-    "workWay": "",
-    "workType": "",
-    "feiyanIsTouch": "否",
-    "isPassGd": "否",
-    "help": "无",
-    "coldReason": "",
-    "coldDate": "",
-    "hospitalName": "",
-    "noWorkReason": "周末",
-    "trainNumber": "",
-    "overseasReason": "",
-    "isolateInfo": ""
-}
-
-const vacation = {
-    "isCold": "否",
-    "seeDoctor": "",
-    "businessTravel": "否",
-    "returnTools": "",
-    "leaveCompanyLocation": "否",
-    "coldFamily": "",
-    "leaveWay": "",
-    "isolate": "否",
-    "isWorking": "否",
-    "familyIsHome": "",
-    "overseas": "否",
-    "temperature": "36.5",
-    "alreadyReturn": "",
-    "name": "周家华",
-    "englishName": "JerryZhou",
-    "phone": "17629269055",
-    "idCard": "612427199107220010",
-    "company": "西安华讯",
-    "department": "STS",
-    "team": "ADV-RND",
-    "location": "陕西省西安市长安区韦曲北街风憬天下西区12号楼",
-    "holidayRegion": "陕西省西安市长安区韦曲北街风憬天下西区12号楼",
-    "workWay": "",
-    "workType": "",
-    "feiyanIsTouch": "否",
-    "isPassGd": "否",
-    "help": "无",
-    "coldReason": "",
-    "coldDate": "",
-    "hospitalName": "",
-    "noWorkReason": "请假",
-    "trainNumber": "",
-    "overseasReason": "",
-    "isolateInfo": ""
-}
-
-const other = {
-    "isCold": "否",
-    "seeDoctor": "",
-    "businessTravel": "否",
-    "returnTools": "",
-    "leaveCompanyLocation": "否",
-    "coldFamily": "",
-    "leaveWay": "",
-    "isolate": "否",
-    "isWorking": "否",
-    "familyIsHome": "",
-    "overseas": "否",
-    "temperature": "36.5",
-    "alreadyReturn": "",
-    "name": "周家华",
-    "englishName": "JerryZhou",
-    "phone": "17629269055",
-    "idCard": "612427199107220010",
-    "company": "西安华讯",
-    "department": "STS",
-    "team": "ADV-RND",
-    "location": "陕西省西安市长安区韦曲北街风憬天下西区12号楼",
-    "holidayRegion": "陕西省西安市长安区韦曲北街风憬天下西区12号楼",
-    "workWay": "",
-    "workType": "",
-    "feiyanIsTouch": "否",
-    "isPassGd": "否",
-    "help": "无",
-    "coldReason": "",
-    "coldDate": "",
-    "hospitalName": "",
-    "noWorkReason": "其他原因",
-    "trainNumber": "",
-    "overseasReason": "",
-    "isolateInfo": ""
-}
-
-exports.health = (type) => {
-    if (!type || isNaN(type)) return null;
-    let info = {};
-    switch (Number(type)) {
-        case 1:
-            // 工作
-            info = Object.assign(info, work);
-            break;
-        case 2:
-            // 周末
-            info = Object.assign(info, weekend);
-            break;
-        case 3:
-            // 请假
-            info = Object.assign(info, vacation);
-            break;
-        case 4:
-            // 其他
-            info = Object.assign(info, other);
-            break;
-        default:
-            info = null
-    }
-    return info;
+exports.quma = {
+    "error_code": 0,
+    "success": true,
+    "result": [
+        {
+            "id": "110000",
+            "name": "北京市",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "120000",
+            "name": "天津市",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "130000",
+            "name": "河北省",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "140000",
+            "name": "山西省",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "150000",
+            "name": "内蒙古自治区",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "210000",
+            "name": "辽宁省",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "220000",
+            "name": "吉林省",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "230000",
+            "name": "黑龙江省",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "310000",
+            "name": "上海市",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "320000",
+            "name": "江苏省",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "330000",
+            "name": "浙江省",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "340000",
+            "name": "安徽省",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "350000",
+            "name": "福建省",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "360000",
+            "name": "江西省",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "370000",
+            "name": "山东省",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "410000",
+            "name": "河南省",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "420000",
+            "name": "湖北省",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "430000",
+            "name": "湖南省",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "440000",
+            "name": "广东省",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "450000",
+            "name": "广西壮族自治区",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "460000",
+            "name": "海南省",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "500000",
+            "name": "重庆市",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "510000",
+            "name": "四川省",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "520000",
+            "name": "贵州省",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "530000",
+            "name": "云南省",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "540000",
+            "name": "西藏自治区",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "610000",
+            "name": "陕西省",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "620000",
+            "name": "甘肃省",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "630000",
+            "name": "青海省",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "640000",
+            "name": "宁夏回族自治区",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "650000",
+            "name": "新疆维吾尔自治区",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "710000",
+            "name": "台湾省",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "810000",
+            "name": "香港",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "820000",
+            "name": "澳门",
+            "fid": "0",
+            "level_id": "1"
+        },
+        {
+            "id": "990000",
+            "name": "苏鲁交界",
+            "fid": "0",
+            "level_id": "1"
+        }
+    ],
 }
